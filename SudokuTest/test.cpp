4ad75dca5470991392ef34c69b2ad1d401b251dc
GuoKunchang <18213857028@163.com>
2023-06-19 20:28:34 +0800

    finale generation complete

--- SudokuTest/test.cpp
+++ SudokuTest/test.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 
-TEST(test_grid_to_str, flat_and_map) {
+TEST(test_grid_to_str, case0) 
+{
 	/*
 	A grid and its flattened string are provided
 	`grid_to_str()` is called to check if 
@@ -38,7 +39,8 @@ TEST(test_grid_to_str, flat_and_map) {
 	}
 }
 
-TEST(test_safe_check, row_col_box_check) {
+TEST(test_safe_check, case0) 
+{
 	/*
 	Some values are set in the first box ( from (0, 0) to (2, 2) )
 	Following tests check whether 
@@ -67,4 +69,75 @@ TEST(test_safe_check, row_col_box_check) {
 	EXPECT_EQ(s.box_safe(grid, 3, 1, 0), true);		// box check: 3 can    be inserted into (1, 0)
 	EXPECT_EQ(s.safe_check(grid, 2, 3, 2), false);	// all check: 2 cannot be inserted into (3, 2)
 	EXPECT_EQ(s.safe_check(grid, 4, 1, 2), true);	// all check: 4 can    be inserted into (1, 2)
+}
+
+TEST(test_get_blank, case0)
+{
+	/*
+	Use a grid set with some values to test search result.
+	*/
+	Sudoku s;
+	int r, c;
+
+	int grid0[9][9] = {
+		/*       0  1  2  3  4  5  6  7  8 */
+		/* 0 */ {0, 1, 0, 0, 0, 0, 0, 0, 0},
+		/* 1 */ {0, 0, 0, 0, 0, 0, 0, 0, 0},
+		/* 2 */ {0, 0, 2, 0, 0, 0, 0, 0, 0},
+		/* 3 */ {0, 0, 0, 0, 0, 0, 0, 0, 0},
+		/* 4 */ {0, 0, 0, 0, 0, 0, 0, 0, 0},
+		/* 5 */ {0, 0, 0, 0, 0, 0, 0, 0, 0},
+		/* 6 */ {0, 0, 0, 0, 0, 0, 0, 0, 0},
+		/* 7 */ {0, 0, 0, 0, 0, 0, 0, 0, 0},
+		/* 8 */ {0, 0, 0, 0, 0, 0, 0, 0, 0},
+	};
+	EXPECT_EQ(s.get_blank(grid0, &r, &c), true);	// a blank at (0, 0) will be found
+	EXPECT_EQ(r, 0);
+	EXPECT_EQ(c, 0);
+
+	int grid1[9][9] = {
+		/*       0  1  2  3  4  5  6  7  8 */
+		/* 0 */ {3, 1, 0, 0, 0, 0, 0, 0, 0},
+		/* 1 */ {0, 0, 0, 0, 0, 0, 0, 0, 0},
+		/* 2 */ {0, 0, 2, 0, 0, 0, 0, 0, 0},
+		/* 3 */ {0, 0, 0, 0, 0, 0, 0, 0, 0},
+		/* 4 */ {0, 0, 0, 0, 0, 0, 0, 0, 0},
+		/* 5 */ {0, 0, 0, 0, 0, 0, 0, 0, 0},
+		/* 6 */ {0, 0, 0, 0, 0, 0, 0, 0, 0},
+		/* 7 */ {0, 0, 0, 0, 0, 0, 0, 0, 0},
+		/* 8 */ {0, 0, 0, 0, 0, 0, 0, 0, 0},
+	};
+	EXPECT_EQ(s.get_blank(grid1, &r, &c), true);	// a blank at (0, 2) will be found
+	EXPECT_EQ(r, 0);
+	EXPECT_EQ(c, 2);
+
+	int grid2[9][9] = {
+		/*       0  1  2  3  4  5  6  7  8 */
+		/* 0 */ {3, 1, 4, 2, 5, 6, 7, 8, 9},
+		/* 1 */ {0, 0, 0, 0, 0, 0, 0, 0, 0},
+		/* 2 */ {0, 0, 2, 0, 0, 0, 0, 0, 0},
+		/* 3 */ {0, 0, 0, 0, 0, 0, 0, 0, 0},
+		/* 4 */ {0, 0, 0, 0, 0, 0, 0, 0, 0},
+		/* 5 */ {0, 0, 0, 0, 0, 0, 0, 0, 0},
+		/* 6 */ {0, 0, 0, 0, 0, 0, 0, 0, 0},
+		/* 7 */ {0, 0, 0, 0, 0, 0, 0, 0, 0},
+		/* 8 */ {0, 0, 0, 0, 0, 0, 0, 0, 0},
+	};
+	EXPECT_EQ(s.get_blank(grid2, &r, &c), true);	// a blank at (0, 2) will be found
+	EXPECT_EQ(r, 1);
+	EXPECT_EQ(c, 0);
+
+	int grid3[9][9] = {
+		/*       0  1  2  3  4  5  6  7  8 */
+		/* 0 */ {1, 1, 1, 1, 1, 1, 1, 1, 1},
+		/* 1 */ {1, 1, 1, 1, 1, 1, 1, 1, 1},
+		/* 2 */ {1, 1, 1, 1, 1, 1, 1, 1, 1},
+		/* 3 */ {1, 1, 1, 1, 1, 1, 1, 1, 1},
+		/* 4 */ {1, 1, 1, 1, 1, 1, 1, 1, 1},
+		/* 5 */ {1, 1, 1, 1, 1, 1, 1, 1, 1},
+		/* 6 */ {1, 1, 1, 1, 1, 1, 1, 1, 1},
+		/* 7 */ {1, 1, 1, 1, 1, 1, 1, 1, 1},
+		/* 8 */ {1, 1, 1, 1, 1, 1, 1, 1, 1},
+	};
+	EXPECT_EQ(s.get_blank(grid3, &r, &c), false);	// no blank
 }
